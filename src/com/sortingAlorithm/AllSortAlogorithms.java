7eab22714b1d99b5109399f0c4b3382d8fff9f88
NAVEENA AM <naveena.am95@gmail.com>
2019-07-23 00:06:56 +0530

    Updated

--- src/com/sortingAlorithm/AllSortAlogorithms.java
+++ src/com/sortingAlorithm/AllSortAlogorithms.java
@@ -4,7 +4,7 @@ public interface AllSortAlogorithms {
 	public int [] SelectionSort(int [] array);
 	public int [] BubbleSort(int [] array);
 	public int [] InsertionSort(int [] array);
-	public int [] MergeSort(int [] array);
+	public int [] MergeSort(int [] array,int low,int high);
 	public int [] QuickSort(int [] array);
 	public void PrintArray(int [] array);
 	
